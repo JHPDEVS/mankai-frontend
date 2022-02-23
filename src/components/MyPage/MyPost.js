9d0096bd05697ae4abb532c7f8b4703867ce41c1
suleun <teaaa030@gmail.com>
2022-02-23 19:25:03 +0900

    modal ex

--- src/components/MyPage/MyPost.js
+++ src/components/MyPage/MyPost.js
@@ -2,20 +2,55 @@ import * as React from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
+import { Box, Button, Modal, Typography } from '@mui/material';
 
 
 export default function PostList() {
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
    <div>
       <List sx={{ width: '100%', maxWidth: 750, bgcolor: 'background.paper' }}>
     {postData.map((item)=>(
         <ListItem  key={item.id} >
-        <ListItemText primary={item.title} secondary={item.group} />
+        <ListItemText onClick={handleOpen} primary={item.title} secondary={item.group} />
       </ListItem>
 
     ))}
     </List>
   
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          사진 동호회 회원 모집
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+        각 나라에서 가장 아름답다고 생각되는 사진을 찍어 올려주세요!
+          퍼온 사진은 안됩니다!
+        </Typography>
+      </Box>
+    </Modal>
+
    </div>
   );
 }
