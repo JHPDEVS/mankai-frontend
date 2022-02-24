b2bab548b53a71169ac5f28c7a56bc5c3ef33711
jhpdevs <jhpdevs@g.yju.ac.kr>
2022-02-24 21:03:03 +0900

    update

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -19,12 +19,25 @@ const GET_NOTI_FAILURE = 'GET_NOTI_FAILURE'
 const GET_USERS_PENDING = 'GET_USERS_PENDING'
 const GET_USERS_SUCCESS = 'GET_USERS_SUCCESS'
 const GET_USERS_FAILURE = 'GET_USERS_FAILURE'
-
+const POST_USEREDIT_PENDING = 'POST_USEREDIT_PENDING'
+const POST_USEREDIT_SUCCESS = 'POST_USEREDIT_SUCCESS'
+const POST_USEREDIT_FAILURE = 'POST_USEREDIT_FAILURE'
+const POST_NOTI_PENDING = 'POST_NOTI_PENDING'
+const POST_NOTI_SUCCESS = 'POST_NOTI_SUCCESS'
+const POST_NOTI_FAILURE = 'POST_NOTI_FAILURE'
+const GET_MESSAGE_PENDING = "GET_MESSAGE_PENDING";
+const GET_MESSAGE_SUCCESS = "GET_MESSAGE_SUCCESS";
+const GET_MESSAGE_FAILURE = "GET_MESSAGE_FAILURE";
+const GET_ROOM_PENDING = "GET_ROOM_PENDING";
+const GET_ROOM_SUCCESS = "GET_ROOM_SUCCESS";
+const GET_ROOM_FAILURE = "GET_ROOM_FAILURE";
+const ADD_MESSAGE = "ADD_MESSAGE";
 const initialState = {
   user: null,
   pending: false,
   error: false,
   noti: null,
+  rooms: null,
 }
 
 export default handleActions(
@@ -100,8 +113,9 @@ export default handleActions(
       return {
         ...state,
         users_pending: false,
-        users: action.payload,
+        users: action.payload.users,
         users_error: false,
+        users_message: action.payload.message,
       }
     },
     [GET_USERS_FAILURE]: (state, action) => {
@@ -154,6 +168,99 @@ export default handleActions(
         register_pending: false,
         register_error: action.payload,
         error: false,
+      }
+    },
+
+    [POST_USEREDIT_PENDING]: (state, action) => {
+      return {
+        ...state,
+        useredit_pending: true,
+        useredit_error: false,
+      }
+    },
+    [POST_USEREDIT_SUCCESS]: (state, action) => {
+      return {
+        ...state,
+        useredit_register_pending: false,
+        useredit: action.payload,
+      }
+    },
+    [POST_USEREDIT_FAILURE]: (state, action) => {
+      return {
+        ...state,
+        useredit_pending: false,
+        useredit_error: action.payload,
+      }
+    },
+    [POST_NOTI_PENDING]: (state, action) => {
+      return {
+        ...state,
+        post_noti_pending: true,
+        post_noti_error: false,
+      }
+    },
+    [POST_NOTI_SUCCESS]: (state, action) => {
+      return {
+        ...state,
+        post_noti_pending: false,
+        post_noti: action.payload,
+      }
+    },
+    [POST_NOTI_FAILURE]: (state, action) => {
+      return {
+        ...state,
+        post_noti_pending: false,
+        post_noti_error: action.payload,
+      }
+    },
+    [GET_MESSAGE_PENDING]: (state, action) => {
+      return {
+        ...state,
+        message_pending: true,
+        get_message_error: true,
+      }
+    },
+    [GET_MESSAGE_SUCCESS]: (state, action) => {
+      return {
+        ...state,
+        message_pending: false,
+        message: action.payload,
+        get_message_error: false,
+      }
+    },
+    [GET_MESSAGE_FAILURE]: (state, action) => {
+      return {
+        ...state,
+        message_pending: false,
+        get_message_error: true,
+      }
+    },
+    [GET_ROOM_PENDING]: (state, action) => {
+      return {
+        ...state,
+        room_pending: true,
+        get_room_error: true,
+      }
+    },
+    [GET_ROOM_SUCCESS]: (state, action) => {
+      return {
+        ...state,
+        room_pending: false,
+        rooms: action.payload,
+        get_room_error: false,
+      }
+    },
+    [GET_ROOM_FAILURE]: (state, action) => {
+      return {
+        ...state,
+        room_pending: false,
+        get_room_error: true,
+      }
+    },
+    [ADD_MESSAGE]: (state, action) => {
+      return {
+        ...state,
+        message:[...state.message,action.payload.message]
       }
     },
   },
